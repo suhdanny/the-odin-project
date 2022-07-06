6960a83a764064c00be1f7691a4006a0076a3c63
suhdanny <suhdong21@gmail.com>
2022-07-06 14:50:05 -0400

    Implement Header componenet

--- memory-card/src/components/Header.js
+++ memory-card/src/components/Header.js
@@ -4,6 +4,9 @@ export default function Header() {
     return (
         <div className="header">
             <img src="images/header-logo.png" alt="logo" />
+            <div className="outer-circle">
+                <div className="inner-circle"></div>
+            </div>
         </div>
     );
 }
