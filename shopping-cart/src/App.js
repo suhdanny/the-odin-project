3a50e6be4386e68253d282297e52781db721853f
suhdanny <suhdong21@gmail.com>
2022-07-11 21:19:23 -0400

    Set routing paths for each page

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 
 const App = () => {
 	return (
 		<div className='font-sans'>
 			<Header />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='/shop' element={<Shop />} />
+				<Route path='/cart' element={<Cart />} />
+			</Routes>
 		</div>
 	);
 };
--- shopping-cart/src/components/Header.js
+++ shopping-cart/src/components/Header.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { AiOutlineHeart, AiOutlineShopping, AiOutlineShoppingCart } from 'react-icons/ai';
+import { AiOutlineUser, AiOutlineShopping, AiOutlineShoppingCart } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 function Header() {
 	return (
 		<header>
-			<ul className='flex justify-around items-center px-2 py-4'>
-				<li className='uppercase font-bold text-4xl cursor-pointer'>Musinsa</li>
+			<ul className='flex justify-around items-center py-4'>
+				<Link to='/'>
+					<li className='uppercase font-bold text-4xl cursor-pointer'>Fashion.</li>
+				</Link>
 				<div className='flex gap-7 text-xl items-center'>
-					<li className='cursor-pointer'>
-						<AiOutlineHeart size={30} style={{ fill: 'red' }} />
-					</li>
-					<li className='cursor-pointer'>
-						<AiOutlineShopping size={30} />
-					</li>
-					<li className='cursor-pointer'>
-						<AiOutlineShoppingCart size={30} />
-					</li>
+					<Link to='/shop'>
+						<li className='cursor-pointer'>
+							<AiOutlineShopping size={30} />
+						</li>
+					</Link>
+					<Link to='/cart'>
+						<li className='cursor-pointer'>
+							<AiOutlineShoppingCart size={30} />
+						</li>
+					</Link>
+					<Link to='/'>
+						<li className='cursor-pointer'>
+							<AiOutlineUser size={30} />
+						</li>
+					</Link>
 				</div>
 			</ul>
 		</header>
