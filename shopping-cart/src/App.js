8e1d37a0deb5a59afdb3887b3bbec5a8f619b3b7
suhdanny <suhdong21@gmail.com>
2022-07-12 19:38:14 -0400

    Add Footer component using DaisyUI

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -4,11 +4,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Cart from './pages/Cart';
+import Footer from './components/Footer';
 import ScrollTop from './ScrollTop';
 
 const App = () => {
 	return (
-		<div className='font-sans px-40 py-8'>
+		<div className='font-sans px-40 py-8 flex flex-col justify-between min-h-screen gap-5'>
 			<Header />
 			<ScrollTop>
 				<Routes>
@@ -17,6 +18,7 @@ const App = () => {
 					<Route path='/cart' element={<Cart />} />
 				</Routes>
 			</ScrollTop>
+			<Footer />
 		</div>
 	);
 };
