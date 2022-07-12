bb53b7bc62c4f6c6e1e34fe04ebff006a02cf736
suhdanny <suhdong21@gmail.com>
2022-07-12 10:13:43 -0400

    Implement favorited product heart icon

--- shopping-cart/src/components/Item.js
+++ shopping-cart/src/components/Item.js
@@ -1,11 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Context } from '../Context';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 function Item({ item }) {
 	const { addToCart } = useContext(Context);
+	const [hovered, setHovered] = useState(false);
+	const [favorite, setFavorite] = useState(false);
+
+	function heartIcon() {
+		if (!favorite) {
+			if (hovered) {
+				return (
+					<AiOutlineHeart
+						size={25}
+						style={{ fill: 'red', position: 'absolute', top: 20, right: 20, cursor: 'pointer' }}
+						onClick={() => setFavorite(prev => !prev)}
+					/>
+				);
+			}
+		} else {
+			return (
+				<AiFillHeart
+					size={25}
+					style={{ fill: 'red', position: 'absolute', top: 20, right: 20, cursor: 'pointer' }}
+					onClick={() => setFavorite(prev => !prev)}
+				/>
+			);
+		}
+	}
 
 	return (
-		<div className='flex flex-col justify-center items-center gap-4 rounded-lg p-8'>
+		<div
+			className='flex flex-col justify-center items-center gap-4 rounded-lg p-8 relative'
+			onMouseEnter={() => setHovered(true)}
+			onMouseLeave={() => setHovered(false)}>
+			{heartIcon()}
 			<img className='h-60' src={item.image} alt={'product'} />
 			<div className='font-bold text-center'>{item.title}</div>
 			<div className='font-bold'>${item.price}</div>
