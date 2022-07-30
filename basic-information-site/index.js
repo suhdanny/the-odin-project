623f8af2b06f07ba603a382c8d2463e76bd771ac
suhdanny <suhdong21@gmail.com>
2022-07-30 14:06:32 -0400

    Add route to 404 page with status code 400

--- basic-information-site/index.js
+++ basic-information-site/index.js
@@ -16,6 +16,10 @@ app.get('/contact', (req, res) => {
 	res.sendFile(path.resolve('./contact.html'));
 });
 
+app.all('*', (req, res) => {
+	res.status(400).sendFile(path.resolve('./404.html'));
+});
+
 app.listen(3000, () => {
 	console.log('Server is running on port 3000...');
 });
